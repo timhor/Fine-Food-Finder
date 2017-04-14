88ba00a700c8a862597a0b9396a1aebf2476f96e
Soham Patel <z5115401@student.unsw.edu.au>
2017-04-14 15:11:54 +1000

    Add Search Functionality with Clock Fix

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -15,3 +15,16 @@ angular.module('fineFoodFinderApp')
       'Karma'
     ];
   });
+
+$(document).ready(function () {
+$('.nav li a').click(function(e) {
+
+    $('.nav li').removeClass('active');
+
+    var $parent = $(this).parent();
+    if (!$parent.hasClass('active')) {
+        $parent.addClass('active');
+    }
+
+});
+});
