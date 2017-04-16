8a06521d93ebb5b557b34f35736492208871d2a6
Tim Hor <timothy.hor@outlook.com>
2017-04-16 20:43:39 +1000

    Implement user sign up

--- app/scripts/controllers/signup.js
+++ app/scripts/controllers/signup.js
@@ -1,14 +1,28 @@
 'use strict';
 
 angular.module('fineFoodFinderApp')
-  .controller('SignUpCtrl', function($scope) {
+  .controller('SignUpCtrl', function($scope, $http, $log, $window, $location) {
         $scope.list = [];
         $scope.submit = function() {
-          if ($scope.email) {
-            $scope.list.push(this.username);
-            $scope.list.push(this.password1);
-            $scope.list.push(this.password2);
-            $scope.list.push(this.email);
+          if ($scope.password1 === $scope.password2) {
+            $http.get('http://localhost:3000/users').then(function(response) {
+              let numUsers = response.data.length;
+              let newUser = {
+                "id": parseInt(numUsers) + 1,
+                "username": $scope.username,
+                "password": $scope.password1,
+                "email": $scope.email
+              };
+              $http.post('http://localhost:3000/users', JSON.stringify(newUser)).then(
+                function(response) {
+                  $log.log($scope.username + " (" + $scope.email + ") signed up");
+                  $location.path('/login');
+                }, function(response) {
+                  $log.error("Error creating new user");
+                });
+            });
+          } else {
+            $window.alert("Error: passwords don't match");
           }
         };
   });
