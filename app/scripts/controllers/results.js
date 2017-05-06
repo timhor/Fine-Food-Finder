a205f73fe7bec08cd6f23cda950bc50c80637334
mqtran01 <michaelqtran01@gmail.com>
2017-05-06 21:21:02 +1000

    Added manual filter and search, used for verification

--- app/scripts/controllers/results.js
+++ app/scripts/controllers/results.js
@@ -4,7 +4,33 @@ angular.module('fineFoodFinderApp')
   .controller('resultsCtrl', function($scope, $routeParams, $location, $http){
   		$scope.query = $routeParams.query;
         $http.get('http://localhost:3000/restaurants').then(function(response) {
-          $scope.restdata = response.data;
+          //$scope.restdata = response.data;
+          $scope.restdata = [];
+          angular.forEach(response.data, function(value) {
+            console.log(value.name + " has address " + value.address);
+            console.log($scope.query.address)
+            if (value.address.toLowerCase().includes($scope.query.address.toLowerCase())) {
+                $scope.restdata.push(value);
+            }
+          });
+          var listLen = $scope.restdata.length;
+          // Please verify this
+          var i = listLen-1;
+          while (i > 0) {
+            var j  = 0;
+            while (j < i) {
+                if ($scope.restdata[j].postcode > $scope.restdata[j+1].postcode) {
+                    var temp = $scope.restdata[j];
+                    $scope.restdata[j] = $scope.restdata[j+1];
+                    $scope.restdata[j+1] = temp;
+
+                }
+                j += 1;
+            }
+            i -= 1;
+
+          }
+
 	    });
     });
 
