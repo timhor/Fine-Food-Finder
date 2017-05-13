5f34bc73667e598686627add7d93ceb335940b20
Tim Hor <timothy.hor@outlook.com>
2017-05-13 19:15:12 +1000

    Handle case where $scope.query.address is undefined (fixes issue #2)

--- app/scripts/controllers/results.js
+++ app/scripts/controllers/results.js
@@ -1,41 +1,43 @@
 'use strict';
 
 angular.module('fineFoodFinderApp')
-  .controller('resultsCtrl', function($scope, $routeParams, $location, $http){
-  		$scope.query = $routeParams.query;
-        $http.get('http://localhost:3000/restaurants').then(function(response) {
-          	if (console.log(!$scope.query)) {
-          		$scope.restdata = response.data;
-          	} else {
-          		$scope.restdata = [];
-	          	angular.forEach(response.data, function(value) {
-	            	console.log(value.name + " has address " + value.address);
-	            	console.log($scope.query.address);
-	            	if (value.address.toLowerCase().includes($scope.query.address.toLowerCase())) {
-	                	$scope.restdata.push(value);
-	            	}
-	          	});
-		        var listLen = $scope.restdata.length;
-		        // Please verify this
-		        var i = listLen-1;
-		        while (i > 0) {
-		        var j  = 0;
-		        	while (j < i) {
-		                if ($scope.restdata[j].postcode > $scope.restdata[j+1].postcode) {
-		                    var temp = $scope.restdata[j];
-		                    $scope.restdata[j] = $scope.restdata[j+1];
-		                    $scope.restdata[j+1] = temp;
+  .controller('resultsCtrl', function($scope, $routeParams, $location, $http, $log){
+    $scope.query = $routeParams.query;
+    $http.get('http://localhost:3000/restaurants').then(function(response) {
+      if ($log.log(!$scope.query)) {
+        $scope.restdata = response.data;
+      } else {
+        $scope.restdata = [];
+        angular.forEach(response.data, function(value) {
+          if ($scope.query.address != undefined) {
+            $log.log(value.name + " has address " + value.address);
+            $log.log("addr: " + $scope.query.address);
+            if (value.address.toLowerCase().includes($scope.query.address.toLowerCase())) {
+              $scope.restdata.push(value);
+            }
+          }
+        });
+        var listLen = $scope.restdata.length;
 
-		                }
-		                j += 1;
-		            }
-		            i -= 1;
+        // TODO please verify this
+        var i = listLen-1;
+        while (i > 0) {
+        var j  = 0;
+          while (j < i) {
+            if ($scope.restdata[j].postcode > $scope.restdata[j+1].postcode) {
+              var temp = $scope.restdata[j];
+              $scope.restdata[j] = $scope.restdata[j+1];
+              $scope.restdata[j+1] = temp;
 
-		        }
-         	}
-	    });
-	    $scope.myFunc = function(value) {
-        	$location.path('/restaurant').search({key: value});
-    	};
+            }
+            j += 1;
+          }
+          i -= 1;
+        }
+      }
     });
+    $scope.viewRestaurant = function(value) {
+      $location.path('/restaurant').search({key: value});
+    };
+  });
 
