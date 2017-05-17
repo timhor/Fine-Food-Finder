a93eb44198674a18c6e998f11dc3e8a3288da0f1
mqtran01 <michaelqtran01@gmail.com>
2017-05-17 20:25:20 +1000

    Fixes to bubble sort and database

--- app/scripts/controllers/results.js
+++ app/scripts/controllers/results.js
@@ -16,21 +16,20 @@ angular.module('fineFoodFinderApp')
         });
         var listLen = $scope.restdata.length;
 
-        // TODO please verify this
-        var i = listLen-1;
-        while (i > 0) {
-        var j  = 0;
-          while (j < i) {
-            if ($scope.restdata[j].rating < $scope.restdata[j+1].rating) {
-              var temp = $scope.restdata[j];
-              $scope.restdata[j] = $scope.restdata[j+1];
-              $scope.restdata[j+1] = temp;
-
+        if ($scope.restdata.length > 1) {
+          var i = 0;
+          while (i < $scope.restdata.length) {
+            var j = $scope.restdata.length - 1;
+            while (j > i) {
+              if ($scope.restdata[j].rating < $scope.restdata[j-1].rating) {
+                var t = $scope.restdata[j];
+                $scope.restdata[j] = $scope.restdata[j-1];
+                $scope.restdata[j-1] = t;
+              }
             }
-            j += 1;
           }
-          i -= 1;
         }
+
     });
     $scope.viewRestaurant = function(value) {
       $location.path('/restaurant').search({restdata: value});
