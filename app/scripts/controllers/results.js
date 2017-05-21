77d83f2ba148f2da5e50cd7bf5e0c1cc31505ce6
VintonyPadmadiredja <vintony.padmadiredja@student.unsw.edu.au>
2017-05-21 18:07:46 +1000

    Fix sort on result page to match arrow buttons and remove unecessary variable initialisation

--- app/scripts/controllers/results.js
+++ app/scripts/controllers/results.js
@@ -3,8 +3,8 @@
 angular.module('fineFoodFinderApp')
   .controller('resultsCtrl', function($scope, $routeParams, $location, $http, $log){
     $scope.query = $routeParams.query;
-    $scope.ratingsCount = 0;
-    $scope.priceCount = 0;
+    $scope.ratingsCount = 1;
+    $scope.priceCount = 1;
     $http.get('http://localhost:3000/restaurants').then(function(response) {
       $scope.restdata = [];
       angular.forEach(response.data, function(value) {
