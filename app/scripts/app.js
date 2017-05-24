58c8a47ae28bd4d4b48944dde05b5e2404f999c0
VintonyPadmadiredja <vintony.padmadiredja@student.unsw.edu.au>
2017-05-24 23:03:26 +1000

    Remove path to AboutCtrl

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -22,9 +22,7 @@ angular
         controllerAs: 'main'
       })
       .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl',
-        controllerAs: 'about'
+        templateUrl: 'views/about.html'
       })
       .when('/login', {
         templateUrl: 'views/login.html',
