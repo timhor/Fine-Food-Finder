39d37ed350eb17a297ff396dfba18565579febad
VintonyPadmadiredja <vintony.padmadiredja@student.unsw.edu.au>
2017-05-27 15:27:21 +1000

    Add Admin menu items

--- app/scripts/controllers/add.js
+++ app/scripts/controllers/add.js
@@ -8,6 +8,14 @@ angular.module('fineFoodFinderApp')
           value3 : false,
           value4 : false
         };
+
+        $scope.items = [{name:"", description:"", price:""}];
+        $scope.addRow = function(){
+          $scope.items.push({name:"", description:"", price:""});
+        };
+        $scope.deleteRow = function(index){
+          $scope.items.splice(index,1);
+        };
         $scope.submit = function() {          
           $http.get('http://localhost:3000/restaurants').then(function(response) {
             let numRests = response.data.length;
@@ -24,13 +32,25 @@ angular.module('fineFoodFinderApp')
               "numRating": 0,
               "diet": $scope.checkboxModel
             };
+
+            let newRestaurantMenu = {
+              "id": parseInt(numRests) + 1,
+              "items": $scope.items
+            }
+
             $http.post('http://localhost:3000/restaurants', JSON.stringify(newRestaurant)).then(
               function(response) {
-                $log.log($scope.name + " added to database");
-                $location.path('#!/');
+              $http.post('http://localhost:3000/menus', JSON.stringify(newRestaurantMenu)).then(
+                function(response) {
+                  $log.log($scope.name + " Menu added to database");
+                  $location.path('#!/');
+                }, function(response) {
+                  $log.error("Error adding new restaurant menu");
+                });
               }, function(response) {
                 $log.error("Error creating new restaurant");
               });
+            
             });
         };
   });
